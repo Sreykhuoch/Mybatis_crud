daf918b71bf1f22266bb6925342fa89abb252362
Administator <khocsrey7@gmail.com>
2024-11-21 09:57:44 +0700

    first commit

---
+++ src/test/java/com/example/mybatis_crud/MyBatisCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatis_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
